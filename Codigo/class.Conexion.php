6a537a9c38687306ae203a4ae63b0d6331bf2cd8
wfcop <wfcop@hotmail.com>
2016-04-14 20:54:55 -0500

    ediciones varias
    arreglos varios y adiciones de nombres a objetos

--- Codigo/class.Conexion.php
+++ Codigo/class.Conexion.php
@@ -6,6 +6,7 @@ class Conexion extends mysqli{
         parent::__construct('localhost','root','QsnY35Y3','db_activos');
         $this->query("SET NAMES 'utf8';");
         $this->connect_errno ? die('Error con la conexion'): $x = 'Conectado';
+        echo $x;
         unset ($x);
         
     }
