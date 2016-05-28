647b2f901fe173c5626f3e06647e1a331652cb0a
ingrid3599 <ingrid3599@hotmail.com>
2016-05-28 00:44:12 -0500

    Implementación
    se sube los archivos de implementación : seguimiento de proyecto y
    burnDown Chart . se crea la carpeta documentación código y se sube la
    documentacion del codigo login y el gestor

---
+++ Codigo/Gestor/html_Gestor/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_5f_5fconstruct',['__construct',['../class_calc_riesgos.html#a5f1270162a0b70785101cd8ebc1670be',1,'CalcRiesgos']]]
+];
---
+++ documentacion/4-Implementación/1-Documentación Codigo/html_Login/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['acceso',['Acceso',['../class_acceso.html',1,'']]],
+  ['anadir',['Anadir',['../class_anadir.html',1,'']]]
+];
